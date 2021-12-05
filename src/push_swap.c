e89981616fbae0c82db549c9d8f6e95c41f0587d
Tamirat worku Lemma <tlemma@3-C-8.42heilbronn.de>
2021-12-05 23:47:17 +0100

    basic algorit added

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tlemma <tlemma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 16:02:03 by tlemma            #+#    #+#             */
-/*   Updated: 2021/12/05 20:35:41 by tlemma           ###   ########.fr       */
+/*   Updated: 2021/12/05 23:38:45 by tlemma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,40 @@ void	ft_error(char *msg)
 
 void	sort_stack(t_stack **a, t_stack **b, int len)
 {
-		
-	push(a, b, 'a');
+	int max;
+	int min;
 
-	comb_oper(rotate, a, b, 'r');		
-	comb_oper(rotate, a, b, 'r');
-
+	if(*b == NULL || (*a)->value > (*b)->value)
+	{
+		if (*b == NULL)
+			push(a, b, 'a');
+		else
+		{
+			max = (*b)->value;
+			while((*a)->value > (*b)->value)
+			{
+				rev_rotate(b, 'b');
+				if ((*b)->value <= max)
+					break ;
+				max = (*b)->value;
+			}
+			rotate(b, 'b');
+			push(a, b, 'a');
+		}
+	}
+	else
+	{
+		min = (*b)->value;
+		while((*a)->value < (*b)->value)
+		{
+			rotate(b, 'b');
+			if ((*b)->value >= min)
+				break ;
+			min = (*b)->value;
+		}
+		push(a, b, 'a');
+	}
+	rotate(a, 'a');
 }
 
 int	main(int argc, char *argv[])
@@ -35,17 +63,20 @@ int	main(int argc, char *argv[])
 
 	b = NULL;
 	a = init_stack(argc - 1, &(argv[1]));
+	print_stacks(a, b);
 	int steps = 0;
-	scanf("%d", &steps);
-	while(steps)
+	// scanf("%d", &steps);
+	while(a != NULL)
 	{
 		sort_stack(&a, &b, argc - 1);
-		steps--;	
-		if(!steps)
-		{
-			print_stacks(a, b);
-			scanf("%d", &steps);
-		}
+		// steps--;	
+		// if(!steps)
+		// {
+		// 	print_stacks(a, b);
+		// 	// scanf("%d", &steps);
+		// }
 	}
+	print_stacks(a, b);
+
 	return (0);
 }
